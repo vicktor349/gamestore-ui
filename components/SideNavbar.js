4e0f4aaf6cfa2de253b5edbcdc285b201c5d2ed7
vicktoh <ayomidevictor349@gmail.com>
2023-12-20 11:59:05 +0100

    Change the hieght to fit the container

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -16,12 +16,13 @@ const SideNavbar = () =>
         setValue(newValue);
     };
     return (
-        <div className='text-[#434f5c]'>
+        <div className='text-[#434f5c] mt-12'>
             <div className="mx-5">
+                <h1 className='mb-6'>CATEGORY</h1>
                 <div>
-                    <div className='flex'>
+                    <div className='flex mb-4'>
                         <h1>PRICE($)</h1>
-                        <h1 className='ml-auto hover:cursor-pointer select-none'>APPLY</h1>
+                        <h1 className='ml-auto hover:cursor-pointer select-none px-2 hover:bg-[rgba(168,195,244,0.99)]'>APPLY</h1>
                     </div>
                     <Slider
                         getAriaLabel={() => 'Temperature range'}
@@ -36,45 +37,54 @@ const SideNavbar = () =>
                         <span className="ml-auto border border-solid border-gray-300 px-8">{value[1]}</span>
                     </div>
                 </div>
-                <div className='select-none'>
-                    <h1 className='font-semibold text-[#434f5c]'>GENRE </h1>
+                <div className='select-none mt-10'>
+                    <h1 className='font-semibold text-[#434f5c] mb-2'>GENRE </h1>
                     {/* CHECKBOX 1 */}
-                    <input type="checkbox" name="fighting" id="fighting" className="hover:cursor-pointer" />
-                    <label htmlFor="fighting" className='hover:cursor-pointer ml-2'>Fighting</label><br />
+                    <div className='mb-2'>
+                        <input type="checkbox" name="fighting" id="fighting" className="hover:cursor-pointer" />
+                        <label htmlFor="fighting" className='hover:cursor-pointer ml-2'>Fighting</label>
+                    </div>
                     {/* CHECKBOX 2 */}
-                    <input type="checkbox" name="roleplaying" className="hover:cursor-pointer" id="roleplaying" />
-                    <label htmlFor="roleplaying" className='hover:cursor-pointer ml-2'>Role Playing</label><br />
+                    <div className='mb-2'>
+                        <input type="checkbox" name="roleplaying" className="hover:cursor-pointer" id="roleplaying" />
+                        <label htmlFor="roleplaying" className='hover:cursor-pointer ml-2'>Role Playing</label>
+                    </div>
                     {/* CHECKBOX 3 */}
-                    <input type="checkbox" name="sports" id="sports" className="hover:cursor-pointer" />
-                    <label htmlFor="sports" className='hover:cursor-pointer ml-2'>Sports</label><br />
+                    <div className='mb-2'>
+                        <input type="checkbox" name="sports" id="sports" className="hover:cursor-pointer" />
+                        <label htmlFor="sports" className='hover:cursor-pointer ml-2'>Sports</label>
+                    </div>
                     {/* CHECKBOX 4 */}
-                    <input type="checkbox" name="arcade" id="arcade" className="hover:cursor-pointer" />
-                    <label htmlFor="arcade" className='hover:cursor-pointer ml-2'>Arcade</label>
+                    <div className='mb-2'>
+                        <input type="checkbox" name="arcade" id="arcade" className="hover:cursor-pointer" />
+                        <label htmlFor="arcade" className='hover:cursor-pointer ml-2'>Arcade</label>
+                    </div>
                 </div>
-                <div>
+                <div className='mt-6'>
+                    <h1 className='mb-3'>PRODUCT RATING</h1>
                     {/* FOUR STARS RATING */}
-                    <div className='flex '>
+                    <div className='flex mb-2 '>
                         <input type="radio" name="rating" value="fourstars" id="fourstars" className='hover:cursor-pointer' />
                         <label htmlFor="fourstars" className='flex hover:cursor-pointer'>
                             <Rating name="read-only" value={ratingValue[3]} readOnly className='items-cneter' />
                         </label>
                     </div>
                     {/* THREE STARS RATING */}
-                    <div className='flex mt-1'>
+                    <div className='flex mt-3'>
                         <input type="radio" name='rating' value="threestars" id="threestars" className='hover:cursor-pointer' />
                         <label htmlFor="threestars" className='flex hover:cursor-pointer'>
                             <Rating name="read-only" className="items-center" value={ratingValue[2]} readOnly />
                         </label>
                     </div>
                     {/* TWO STARS RATING */}
-                    <div className='flex mt-1'>
+                    <div className='flex mt-3'>
                         <input type="radio" name="rating" id="twostars" value="twostars" className='hover:cursor-pointer' />
                         <label htmlFor="twostars" className='flex hover:cursor-pointer'>
                             <Rating name="read-only" value={ratingValue[1]} readOnly className='items-center' />
                         </label>
                     </div>
                     {/* ONE STAR RATING */}
-                    <div className='flex mt-1'>
+                    <div className='flex mt-3'>
                         <input type="radio" name="rating" id="onestar" value="onestar" className='hover:cursor-pointer' />
                         <label htmlFor="onestar" className='flex hover:cursor-pointer'>
                             <Rating name="read-only" value={ratingValue[0]} readOnly className='items-center' />
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,11 @@ export default function Home()
       </Head>
       <main className="font-body mt-20">
         <div className='grid grid-cols-8'>
-          <div className='bg-white w-64'>
-            <div>
-              <SideNavbar />
-            </div>
+          <div className='bg-white w-64 h-fit pb-12'>
+            <SideNavbar />
           </div>
-          <div className='bg-white col-start-3 col-end-9'>
-            <div>
-              <Card />
-            </div>
+          <div className='bg-white col-start-3 col-end-9 h-fit'>
+            <Card />
           </div>
         </div>
       </main>
