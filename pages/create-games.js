967439cd02778f7a05dff97640b949e36addeafb
vicktoh <ayomidevictor349@gmail.com>
2023-12-25 22:25:29 +0100

    Collecting information from db

--- pages/create-games.js
+++ pages/create-games.js
@@ -1,24 +1,61 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from 'next/head';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const createGames = () =>
 {
+    const [formData, setFormData] = useState({
+        name: "",
+        genre: "",
+        price: "",
+        releasedate: "",
+        url: ""
+
+    })
+    const handleChange = (e) =>
+    {
+        const { id, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+    const handleSubmit = async (e) =>
+    {
+        e.preventDefault()
+        const details = { ...formData }
+        try
+        {
+            await axios({
+                method: "POST",
+                url: "http://localhost:5000/games",
+                data: details,
+                headers: { 'Content-Type': 'application/json' }
+            })
+            console.log(details);
+        } catch (err)
+        {
+            console.log(err);
+        }
+    }
     return (
         <>
             <Head>
                 <title>GameStore | Add Games</title>
             </Head>
             <div className='mx-72 mt-20'>
-                <form action="" className='text-[#434f5c]'>
+                <form action="" className='text-[#434f5c]' onSubmit={handleSubmit}>
                     <label htmlFor="name" className='font-semibold'>Game Name</label><br />
                     <input type="text" name="name" id="name"
                         required
                         className='w-full border border-gray-300 border-solid rounded-md p-1 mb-4 focus:outline-none'
                         placeholder='Enter game name'
+                        value={formData.name}
+                        onChange={handleChange}
                     />
                     <br />
                     <label htmlFor="genre" className='font-semibold'>Genre</label><br />
-                    <select name="genre" id="genre" className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4' required>
+                    <select name="genre" id="genre" value={formData.genre} onChange={handleChange} className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4' required>
                         <option value="" >Select an option</option>
                         <option value="fighting">Fighting</option>
                         <option value="roleplaying">Role Playing</option>
@@ -26,11 +63,11 @@ const createGames = () =>
                         <option value="arcade">Arcade</option>
                     </select><br />
                     <label htmlFor="price" className='font-semibold'>Price</label><br />
-                    <input type='number' name='price' id='price' className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' placeholder='Enter price of game' /><br />
+                    <input type='number' name='price' id='price' value={formData.price} onChange={handleChange} className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' placeholder='Enter price of game' /><br />
                     <label htmlFor="releasedate" className='font-semibold'>Release Date</label><br />
-                    <input type="date" className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' name='releasedate' id='releasedate' /><br />
+                    <input type="date" value={formData.releasedate} onChange={handleChange} className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' name='releasedate' id='releasedate' /><br />
                     <label htmlFor="url" className='font-semibold'>Image url</label><br />
-                    <input type="url" name='url' id='url' className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' placeholder='Enter game display picture' /><br />
+                    <input type="url" name='url' id='url' value={formData.url} onChange={handleChange} className='w-full border border-gray-300 border-solid p-1 rounded-md mb-4 focus:outline-none' placeholder='Enter game display picture' /><br />
                     <button type="submit" className='bg-[#2d4b81] border border-solid border-[#2d4b81] rounded-sm text-white py-1 px-4 flex justify-center items-center'>Submit</button>
                 </form>
             </div>
